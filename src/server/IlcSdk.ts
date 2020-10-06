a98f9a3ee9b5c27f12f3008c8acecd12d20ab2de
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-10-06 20:16:20 +0300

    WebpackPluginsFactory added

---
+++ src/server/IlcSdk.ts
@@ -0,0 +1,218 @@
+import {IncomingMessage, ServerResponse} from 'http';
+import * as types from './types';
+import urljoin from 'url-join';
+
+export class IlcSdk {
+    private log: Console;
+    private defaultPublicPath: string;
+    private titleRegex = /<title.*>.*<\/title\s*>/s;
+    private publicPathProperyName: string;
+
+    /**
+     *
+     * @param options
+     * @param options.logger
+     *
+     *   **Default value:** `console`
+     * @param options.publicPath Default value that will be used if no "publicPath" were defined in app's props inside ILC Registry.
+     *
+     *  **Default value:** `/`
+     *
+     * @param options.publicPathProperyName Allows to use other then `publicPath` key for the property that will be used to determine micro frontend's public path.
+     *
+     *  **Default value:** `publicPath`
+     */
+    constructor(options: { logger?: Console; publicPath?: string; publicPathProperyName?: string } = {}) {
+        this.log = options.logger || console;
+        this.defaultPublicPath = options.publicPath || '/';
+        this.publicPathProperyName = options.publicPathProperyName || 'publicPath';
+    }
+
+    /**
+     * Processes incoming request and returns object that can be used to fetch information passed by ILC to the application.
+     */
+    public processRequest(req: IncomingMessage): types.RequestData {
+        const url = this.parseUrl(req);
+        const routerProps = this.parseRouterProps(url);
+        const requestedUrls = this.getRequestUrls(url, routerProps);
+        const passedProps = this.getPassedProps(url);
+
+        let appId: string;
+        if (routerProps.fragmentName) {
+            appId = routerProps.fragmentName;
+        } else {
+            appId = 'dumbId';
+            this.log.warn(`Missing "appId" information for "${url.href}" request. Falling back to dumb ID.`);
+        }
+
+
+
+        return {
+            getCurrentReqUrl: () => requestedUrls.requestUrl,
+            getCurrentBasePath: () => requestedUrls.basePageUrl,
+            getCurrentPathProps: () => passedProps,
+            appId,
+            intl: this.parseIntl(req),
+        };
+    }
+
+    /**
+     * Processes outgoing response and allow conveniently pass information from application back to ILC.
+     *
+     * **WARNING:** this method should be called before response headers were send.
+     */
+    public processResponse(reqData: types.RequestData, res: ServerResponse, data?: types.ResponseData): void {
+        if (!data) {
+            return;
+        }
+        if (res.headersSent) {
+            throw new Error('Unable to set all necessary headers as they were already sent');
+        }
+        if (data.pageTitle) {
+            let title = data.pageTitle;
+            if (!this.titleRegex.test(data.pageTitle)) {
+                title = `<title>${title}</title>`;
+            }
+            res.setHeader('x-head-title', Buffer.from(title, 'utf8').toString('base64'));
+        }
+        if (data.pageMetaTags) {
+            res.setHeader('x-head-meta', Buffer.from(data.pageMetaTags, 'utf8').toString('base64'));
+        }
+        if (data.appAssets) {
+            const publicPath = (reqData.getCurrentPathProps() as any)[this.publicPathProperyName];
+            res.setHeader('Link', this.getLinkHeader(data.appAssets, publicPath));
+        }
+
+        res.setHeader('content-type', 'text/html');
+    }
+
+    /**
+     * Correctly responds to ILC for the assets discovery request.
+     *
+     * **WARNING:** this method should never be used in production if application is running in more than a single instance
+     */
+    public assetsDiscoveryHandler(req: IncomingMessage, res: ServerResponse, appAssets: types.AppAssets) {
+        const url = this.parseUrl(req);
+        const publicPath = this.getPassedProps(url)[this.publicPathProperyName];
+
+        const resData: any = {
+            spaBundle: this.buildLink(appAssets.spaBundle, publicPath),
+            dependencies: {},
+        };
+        if (appAssets.cssBundle) {
+            resData.cssBundle = this.buildLink(appAssets.cssBundle, publicPath);
+        }
+        if (appAssets.dependencies) {
+            for (const k in appAssets.dependencies) {
+                /* istanbul ignore if */
+                if (!appAssets.dependencies.hasOwnProperty(k)) {
+                    continue;
+                }
+
+                resData.dependencies[k] = this.buildLink(appAssets.dependencies[k], publicPath);
+            }
+        }
+
+        res.statusCode = 200;
+        res.setHeader('content-type', 'application/json; charset=utf-8');
+        res.end(JSON.stringify(resData));
+    }
+
+    private parseIntl(req: IncomingMessage) {
+        const intlParams = req.headers['z-intl'] as string|undefined;
+        if (intlParams === undefined) {
+            return null;
+        }
+
+        const paramsParts = intlParams.split(';');
+        const localeParams = paramsParts[0].split(':');
+        const currencyParams = paramsParts[1].split(':');
+
+        return {
+            get: () => ({locale: localeParams[0], currency: currencyParams[0]}),
+            getDefault: () => ({locale: localeParams[1], currency: currencyParams[1]}),
+            getSupported: () => ({locale: localeParams[2].split(','), currency: currencyParams[2].split(',')})
+        }
+    }
+
+    private parseRouterProps(url: URL) {
+        if (url.searchParams.has('routerProps')) {
+            return JSON.parse(Buffer.from(url.searchParams.get('routerProps')!, 'base64').toString('utf-8'));
+        } else {
+            return {};
+        }
+    }
+
+    private getRequestUrls(url: URL, routerProps: any) {
+        const res = {
+            // Base path used for links on the page, should be relative. Can be ignored if memory routing is in use
+            // More info: https://collab.namecheap.net/x/myZdCw
+            // https://github.com/ReactTraining/history/blob/3f69f9e07b0a739419704cffc3b3563133281548/docs/Misc.md#using-a-base-url
+            basePageUrl: '/',
+            requestUrl: '/', // basePageUrl should be deducted from it
+        };
+
+        if (routerProps.basePath && routerProps.reqUrl) {
+            res.basePageUrl = routerProps.basePath;
+            res.requestUrl = urljoin('/', routerProps.reqUrl.replace(routerProps.basePath, ''));
+        } else {
+            this.log.warn(`Missing "routerProps" for "${url.href}" request. Fallback to / & /`);
+        }
+
+        return res;
+    }
+
+    private getPassedProps(url: URL) {
+        if (!url.searchParams.has('appProps')) {
+            return {};
+        }
+
+        try {
+            return JSON.parse(Buffer.from(url.searchParams.get('appProps')!, 'base64').toString('utf-8'));
+        } catch (e) {
+            this.log.warn(`Error while parsing passed props. Falling back to empty object...`, e);
+
+            return {};
+        }
+    }
+
+    private parseUrl(req: IncomingMessage) {
+        return new URL(req.url!, `http://${req.headers.host}`);
+    }
+
+    private getLinkHeader(appAssets: types.AppAssets, publicPath?: string) {
+        const links = [
+            `<${this.buildLink(
+                appAssets.spaBundle,
+                publicPath,
+            )}>; rel="fragment-script"; as="script"; crossorigin="anonymous"`,
+        ];
+
+        if (appAssets.cssBundle) {
+            links.push(`<${this.buildLink(appAssets.cssBundle, publicPath)}>; rel="stylesheet"`);
+        }
+
+        for (const k in appAssets.dependencies) {
+            /* istanbul ignore if */
+            if (!appAssets.dependencies.hasOwnProperty(k)) {
+                continue;
+            }
+
+            links.push(
+                `<${this.buildLink(appAssets.dependencies[k], publicPath)}>; rel="fragment-dependency"; name="${k}"`,
+            );
+        }
+
+        return links.join(',');
+    }
+
+    private buildLink(url: string, publicPath?: string) {
+        if (url.includes('http://') || url.includes('https://')) {
+            return url;
+        }
+
+        const pp = publicPath ? publicPath : this.defaultPublicPath;
+
+        return urljoin(pp, url);
+    }
+}
---
+++ src/server/WebpackPlugins.ts
@@ -0,0 +1,11 @@
+const WrapperPlugin = require('wrapper-webpack-plugin');
+
+export function WebpackPluginsFactory(jsFilesTest = /\.js$/) {
+    return [
+        new WrapperPlugin({
+            test: jsFilesTest, // only wrap output of bundle files with '.js' extension
+            header: '(function(define, window, location){\n',
+            footer: '\n})((window.ILC && window.ILC.define) || window.define, window.ILC.window || window, window.ILC.window ? window.ILC.window.location : window.location);'
+        }),
+    ];
+}

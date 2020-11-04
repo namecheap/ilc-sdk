b47f9b64d26ac3df1ef96d7ddada154ef10ea44f
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-11-04 15:58:52 +0200

    feat: added default adapter implementation

---
+++ src/app/defaultIntlAdapter.ts
@@ -0,0 +1,18 @@
+import {IntlAdapter} from "./commonTypes";
+
+const adapter: IntlAdapter = {
+    config: {
+        default: {locale: 'en-US', currency: 'USD'},
+        supported: {locale: ['en-US'], currency: ['USD']},
+    },
+    get() {
+        return this.config.default;
+    }
+};
+
+const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+if (isBrowser) {
+    adapter.set = () => {}
+}
+
+export default adapter;

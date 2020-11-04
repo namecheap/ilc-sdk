8ea542dbc9952e6219e9466449354920f0d969bd
Vladlen Fedosov <vladlen.f@namecheap.com>
2020-11-04 15:59:24 +0200

    chore: code formatting

--- src/app/defaultIntlAdapter.ts
+++ src/app/defaultIntlAdapter.ts
@@ -1,18 +1,18 @@
-import {IntlAdapter} from "./commonTypes";
+import { IntlAdapter } from './commonTypes';
 
 const adapter: IntlAdapter = {
     config: {
-        default: {locale: 'en-US', currency: 'USD'},
-        supported: {locale: ['en-US'], currency: ['USD']},
+        default: { locale: 'en-US', currency: 'USD' },
+        supported: { locale: ['en-US'], currency: ['USD'] },
     },
     get() {
         return this.config.default;
-    }
+    },
 };
 
 const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
 if (isBrowser) {
-    adapter.set = () => {}
+    adapter.set = () => {};
 }
 
 export default adapter;

0b5ae71e95a62573f65091a6799302e957b266df
Vladlen Fedosov <vladlen.f@namecheap.com>
2021-03-09 21:18:26 +0200

    feat: added ability to detect bundle path automatically via Webpack plugin

--- src/server/WebpackPlugins.ts
+++ src/server/WebpackPlugins.ts
@@ -1,13 +1,55 @@
 /* istanbul ignore file */
 
-export function WebpackPluginsFactory(jsFilesTest = /\.js$/) {
-    const WrapperPlugin = require('wrapper-webpack-plugin');
+export interface FactoryConfig {
+    jsFilesTest?: RegExp;
+    publicPathDetection?: {
+        disable?: boolean;
+        /**
+         * If you need the webpack public path to "chop off" some of the directories in the current module's url, you can specify a "root directory level". Note that the root directory level is read from right-to-left, with `1` indicating "current directory" and `2` indicating "up one directory":
+         * optional: defaults to 1
+         */
+        rootDirectoryLevel?: number;
+        /**
+         * ONLY NEEDED FOR WEBPACK 1-4. Not necessary for webpack@5
+         * example: @portal/appName
+         */
+        systemjsModuleName?: string;
+    };
+}
 
-    return [
+const defaultConf: FactoryConfig = {
+    jsFilesTest: /\.js$/,
+    publicPathDetection: {
+        disable: false,
+    },
+};
+
+export function WebpackPluginsFactory(config: RegExp | FactoryConfig) {
+    const WrapperPlugin = require('wrapper-webpack-plugin');
+    const SystemJSPublicPathWebpackPlugin = require('systemjs-webpack-interop/SystemJSPublicPathWebpackPlugin');
+    const _merge = require('lodash.merge');
+
+    if (config instanceof RegExp) {
+        config = {
+            jsFilesTest: config,
+        };
+    }
+
+    const conf: FactoryConfig = _merge({}, defaultConf, config);
+
+    const plugins = [
         new WrapperPlugin({
-            test: jsFilesTest, // only wrap output of bundle files with '.js' extension
+            test: conf.jsFilesTest, // only wrap output of bundle files with '.js' extension
             header: '(function(define){\n',
             footer: '\n})((window.ILC && window.ILC.define) || window.define);',
         }),
     ];
+
+    if (conf.publicPathDetection && conf.publicPathDetection.disable) {
+        return plugins;
+    }
+
+    plugins.push(new SystemJSPublicPathWebpackPlugin({ ...conf.publicPathDetection }));
+
+    return plugins;
 }

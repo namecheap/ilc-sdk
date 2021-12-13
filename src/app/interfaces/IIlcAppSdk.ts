54e052afc37d49054c296f58bc46a9b65645b178
Volodymyr Makukha <nei.css@gmail.com>
2021-12-13 16:23:10 +0200

    fix: type of IIlcAppSdk

--- src/app/interfaces/IIlcAppSdk.ts
+++ src/app/interfaces/IIlcAppSdk.ts
@@ -1,7 +1,9 @@
 import { IlcIntl } from '../IlcIntl';
+import { Render404 } from '../interfaces/common';
 
 export interface IIlcAppSdk {
     /** Unique application ID, if same app will be rendered twice on a page - it will get different IDs */
     appId: string;
     intl: IlcIntl;
+    render404: Render404;
 }

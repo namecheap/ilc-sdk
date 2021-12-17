588c5c404962b01edd651bc01f779f068ee08ee7
Volodymyr Makukha <nei.css@gmail.com>
2021-12-17 17:22:37 +0200

    test: IlcAppSdk

---
+++ test/app/IlcAppSdk.spec.ts
@@ -0,0 +1,35 @@
+import IlcAppSdk from '../../src/app/index';
+import { expect } from 'chai';
+
+describe('IlcAppSdk', () => {
+    it('should throw error due to not provided adapter', () => {
+        // @ts-ignore
+        expect(() => new IlcAppSdk()).to.throw('Unable to determine adapter properly...');
+    });
+
+    it('should render404 run trigger404Page method from adapter', () => {
+        let page404Rendered = false;
+
+        const appSdk = new IlcAppSdk({
+            appId: 'someAppId',
+            intl: null,
+            trigger404Page: () => {
+                page404Rendered = true;
+            },
+        });
+
+        expect(page404Rendered).to.be.false;
+        appSdk.render404();
+        expect(page404Rendered).to.be.true;
+    });
+
+    it('should "unmount" throw error due to not defined set method in adapter', () => {
+        const appSdk = new IlcAppSdk({
+            appId: 'someAppId',
+            intl: null,
+            trigger404Page: () => {},
+        });
+
+        expect(() => appSdk.unmount()).to.throw("Looks like you're trying to call CSR only method during SSR");
+    });
+});

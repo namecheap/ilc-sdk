5b4d54bcece71919c685cb1dedec9d656e8e10a5
Stanislav Mishchyshyn <stanislav.mishchyshyn@namecheap.com>
2023-11-30 13:05:13 +0200

    fix: add manifest property to external interface

--- src/app/interfaces/IIlcAppSdk.ts
+++ src/app/interfaces/IIlcAppSdk.ts
@@ -1,9 +1,11 @@
 import { IlcIntl } from '../IlcIntl';
 import { Render404 } from '../interfaces/common';
+import { ApplicationConfig } from './AppConfig';
 
 export interface IIlcAppSdk {
     /** Unique application ID, if same app will be rendered twice on a page - it will get different IDs */
     appId: string;
     intl: IlcIntl;
     render404: Render404;
+    manifest?: ApplicationConfig;
 }

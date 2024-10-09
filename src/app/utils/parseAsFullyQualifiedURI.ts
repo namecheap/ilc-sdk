4227e5c853b930c905d06158a83e94683d8e85fb
Nikita Tkalenko <nikita.tkalenko@namecheap.com>
2024-10-09 09:50:27 +0300

    chore: slightly change implementation to utilise url object due to missleading test case.

--- src/app/utils/parseAsFullyQualifiedURI.ts
+++ src/app/utils/parseAsFullyQualifiedURI.ts
@@ -1,11 +1,12 @@
 export default function parseAsFullyQualifiedURI(uri: string) {
     let origin = '';
     try {
-        // Normalize multiple slashes to a single slash, but don't affect the initial "http://" or "https://"
-        uri = uri.replace(/([^:])\/{2,}/g, '$1/');
-
         const urlObj = new URL(uri);
         origin = urlObj.origin;
+
+        // Apply replacement only to the pathname, leaving the rest (search, hash) intact
+        urlObj.pathname = urlObj.pathname.replace(/\/{2,}/g, '/');
+
         uri = urlObj.pathname + urlObj.search + urlObj.hash;
     } catch {}
 

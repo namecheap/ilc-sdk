6238fcf0b7742f60039d73adf57f6ef99d39c914
Stanislav Mishchyshyn <stanislav.mishchyshyn@namecheap.com>
2024-10-17 16:53:14 +0300

    ci: add auto bump, change log and publish

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,6 @@
+/**
+ * @type {import('lint-staged').Config}
+ */
 module.exports = {
     '*': 'prettier --ignore-unknown --write',
     '*.ts': 'npm run lint -- --fix',
---
+++ release.config.js
@@ -0,0 +1,6 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+    branches: ['master', 'feature/publish-package'],
+};
